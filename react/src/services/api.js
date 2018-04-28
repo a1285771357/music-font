15b814acb6d99b2c5bf9aba5bac803e22e27829f
zhanglei <zhanglei@baofeng.com>
2018-04-28 17:18:31 +0800

    编写断点续播demo

--- react/src/services/api.js
+++ react/src/services/api.js
@@ -2,7 +2,7 @@ import request from '../utils/request';
 //用户登陆ok
 export async function goLogin(params) {
   return request({//将数据结果返回
-    url: 'http://192.168.130.250:3001/',
+    url: 'http://192.168.130.158:3001/serchfile',
     method: 'get',
     data : params
   });
